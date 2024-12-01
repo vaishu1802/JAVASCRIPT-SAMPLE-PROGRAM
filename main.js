ce7972f1631eaea574dc9e5a0867de7a874e3b7f
VAISHUNAVI <149314882+vaishu1802@users.noreply.github.com>
2024-11-30 23:28:25 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById('joinBtn').addEventListener('click',function(){
+    document.getElementById('result').innerHTML= (document.getElementById('firstname').value  +  document.getElementById('lastname').value)
+})
